f399869903b94d4df192dc4aaf171dda77c08314
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-04-16 22:34:21 +0530

    Time: 2 ms (98.83%), Space: 44.4 MB (95.36%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -3,10 +3,10 @@ class Solution {
         char[] arr=s.toCharArray();
         int l=0;
         int h=s.length()-1;
-        while(l<=h){
-            if(!(Character.toString(arr[l])).matches(".*[aeiouAEIOU].*")){
+        while(l<h){
+            if(arr[l]!='a'&&arr[l]!='e'&&arr[l]!='i'&&arr[l]!='o'&&arr[l]!='u'&&arr[l]!='A'&&arr[l]!='E'&&arr[l]!='I'&&arr[l]!='O'&&arr[l]!='U'){
                 l++;
-            }else if(!(Character.toString(arr[h])).matches(".*[aeiouAEIOU].*")){
+            }else if(arr[h]!='a'&&arr[h]!='e'&&arr[h]!='i'&&arr[h]!='o'&&arr[h]!='u'&&arr[h]!='A'&&arr[h]!='E'&&arr[h]!='I'&&arr[h]!='O'&&arr[h]!='U'){
                 h--;
             }else{
                 char c=arr[l];
