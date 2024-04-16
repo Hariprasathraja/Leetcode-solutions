df6c406077b054cdf5405ecee963cda1d6ea2bab
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-04-16 18:57:28 +0530

    Time: 6 ms (44.83%), Space: 42.9 MB (83.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())return false;
-        int[] arr= new int[26];
+        int[] arr= new int[27];
         for(int i=0; i<s.length(); i++){
             arr[s.charAt(i)-'a']++;
             arr[t.charAt(i)-'a']--;
