7215f82153f8859567d25e06911f415067a2678d
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-04-24 10:34:17 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (50.26%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,15 +1,21 @@
 class Solution {
+    private int[] map={2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
     public String[] findWords(String[] words) {
     List<String>list=new ArrayList<>();
     for(String i:words){
-        if(!i.matches(".*[^QWERTYUIOPqwertyuiop].*")){
-            list.add(i);
-        }else if(!i.matches(".*[^ASDFGHJKLasdfghjkl].*")){
-            list.add(i);
-        }else if(!i.matches(".*[^ZXCVBNMzxcvbnm].*")){
+        if(check(i.toLowerCase())){
             list.add(i);
         }
     }
     return list.toArray(new String[list.size()]);
+    }
+    private boolean check(String st){
+        int r=map[st.charAt(0)-'a'];
+        for(char c:st.toCharArray()){
+            if(map[c-'a']!=r){
+                return false;
+            }
+        }
+        return true;
     }
 }
