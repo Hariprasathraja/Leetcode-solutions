35e71eff4a4c85d0ef22dca4d2c89d2cb9af6eb6
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-04-27 21:27:45 +0530

    Time: 20 ms (90.03%), Space: 54.5 MB (25.44%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,18 +1,15 @@
 class RecentCounter {
     Queue<Integer> queue;
-    int len=0;
     public RecentCounter() {
         queue=new LinkedList<>();
     }
     
     public int ping(int t) {
         queue.add(t);
-        len++;
         while(queue.peek()<t-3000){
             queue.remove();
-            len--;
         }
-        return len;
+        return queue.size();
     }
 }
 
