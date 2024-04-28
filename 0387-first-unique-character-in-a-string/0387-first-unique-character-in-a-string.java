108fdb1767e407f322ad8a5bf5e0a08395b4c545
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-04-28 21:43:05 +0530

    Time: 4 ms (99.51%), Space: 45.1 MB (39.21%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,11 +1,19 @@
 class Solution {
     public int firstUniqChar(String s){
-        for(char i:s.toCharArray()){
-            int ind=s.indexOf(i);
-            if(ind==s.lastIndexOf(i)){
-                return ind;
+        int ans = Integer.MAX_VALUE;
+        // Iterate from a to z which is 26 which makes it constant
+        for(char c='a'; c<='z';c++){
+            // indexOf will return first index of alphabet and lastIndexOf will return last index
+            // if both are equal then it has occured only once.
+            // through this we will get all index's which are occured once
+            // but our answer is lowest index
+            int index = s.indexOf(c);
+            if(index!=-1&&index==s.lastIndexOf(c)){
+                ans = Math.min(ans,index);
             }
         }
-        return -1;
+
+        // If ans remain's Integer.MAX_VALUE then their is no unique character
+        return ans==Integer.MAX_VALUE?-1:ans;
     }
 }
