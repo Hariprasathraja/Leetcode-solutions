4ebf86c85d6ce3c3edc739e508fdb7848522fa36
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-05-21 15:59:41 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (29.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        quicksort(nums,0,nums.length-1);
-    }
-    private void quicksort(int[] n,int low,int high){
-        if(low<high){
-            int pivotindx=partition(n,low,high);
-            quicksort(n,low,pivotindx-1);
-            quicksort(n,pivotindx+1,high);
-        }
-    }
-    private int partition(int[] arr,int low,int high){
-        int pivot=arr[high];
-        int i=low-1;
-        for(int j=low;j<high;j++){
-            if(arr[j]<=pivot){
-                i++;
-                int tem=arr[i];
-                arr[i]=arr[j];
-                arr[j]=tem;
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums,mid,high);
+                high--;
             }
         }
-        i++;
+    }
+    private void swap(int[] arr,int i,int j){
         int tem=arr[i];
-        arr[i]=arr[high];
-        arr[high]=tem;
-        
-        return i;
+        arr[i]=arr[j];
+        arr[j]=tem;
     }
 }
