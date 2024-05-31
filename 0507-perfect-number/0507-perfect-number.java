808ec04917b718a018995b6decf4d0ed151da650
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-05-31 23:09:17 +0530

    Time: 2 ms (81.89%), Space: 40.2 MB (62.93%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -5,8 +5,7 @@ class Solution {
         int sum=1;
         for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i==0){
-                sum+=i;
-                sum+=num/i;
+                sum+=i+num/i;
             }
         }
     return sum==num;
