500c1ddd29c41b97c31d2cb8f4d3a242c1cc061a
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-07-05 23:59:56 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (6.31%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,8 +1,9 @@
 class Solution {
     public int minimumMoves(String s) {
         int c=0,i=0;
-        while(i<s.length()){
-            if(s.charAt(i)=='X'){
+        char[] ar=s.toCharArray();
+        while(i<ar.length){
+            if(ar[i]=='X'){
                 c++;
                 i+=3;
             }else i++;
