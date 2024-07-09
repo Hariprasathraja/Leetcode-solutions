1d1ce72b96aa4cbfa96f082a054d1c5b98ee3d78
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-07-09 15:38:10 +0530

    Time: 3 ms (93.06%), Space: 72.8 MB (32.65%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.java
+++ 1701-average-waiting-time/1701-average-waiting-time.java
@@ -1,8 +1,9 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double time=0,totalWaitTime=0;    
+        int time=-1;
+        double totalWaitTime=0;    
         for(int[] i:customers){
-            time=Math.max(time,i[0]);
+            time=time<i[0]? i[0]:time;
             time+=i[1];
             totalWaitTime+=(time-i[0]);
         }
