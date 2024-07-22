ddb46826ccb6035973752f07931da9e0a56f2b22
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-07-22 15:34:52 +0530

    Time: 7 ms (90.64%), Space: 45.3 MB (32.21%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,7 +1,7 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         int n=names.length;
-       /* Queue<int[]>queue=new PriorityQueue<>((a,b)->b[1]-a[1]);
+        Queue<int[]>queue=new PriorityQueue<>((a,b)->b[1]-a[1]);
         for(int i=0;i<n;i++){
             queue.offer(new int[]{i,heights[i]});
         }
@@ -11,8 +11,8 @@ class Solution {
             sorted[indx++]=names[queue.poll()[0]];
         }
         return sorted;
-        */
-        TreeMap<Integer,String> map=new TreeMap<>(Collections.reverseOrder());
+        
+        /*TreeMap<Integer,String> map=new TreeMap<>(Collections.reverseOrder());
         for(int i=0;i<n;i++){
             map.put(heights[i],names[i]);
         }
@@ -21,6 +21,6 @@ class Solution {
         for(int i: map.keySet()){
             sorted[indx++]=map.get(i);
         }
-        return sorted;
+        return sorted;*/
     }
 }
