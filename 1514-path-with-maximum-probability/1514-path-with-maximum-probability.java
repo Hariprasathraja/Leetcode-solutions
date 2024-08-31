7c97494c51dec088cf8b8cdab7dea74ba04a1d62
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-08-31 11:28:39 +0530

    Time: 33 ms (43.55%), Space: 57.5 MB (33.02%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -1,36 +1,34 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
         Map<Integer,List<double[]>> graph=new HashMap<>();
-        for(int i=0;i<n;i++){
-            graph.put(i,new ArrayList<>());
-        }
+        for(int i=0;i<n;i++) graph.put(i,new ArrayList<>());
+        
         for(int i=0;i<edges.length;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            double weight=succProb[i];
-            graph.get(u).add(new double[]{v,weight});
-            graph.get(v).add(new double[]{u,weight});
+            int u=edges[i][0], v=edges[i][1];
+            double w=succProb[i];
+            graph.get(u).add(new double[]{v,w});
+            graph.get(v).add(new double[]{u,w});
         }
-            
-        double[] maxProb=new double[n];
-        maxProb[start_node]=1.0;
+        
         Queue<double[]> maxHeap=new PriorityQueue<>((a,b)->Double.compare(b[1],a[1]));
         maxHeap.offer(new double[]{start_node,1.0});
         
+        double[] allProb=new double[n];
+        allProb[start_node]=1.0;
+        
         while(!maxHeap.isEmpty()){
-            double[] node=maxHeap.poll();
-            int curNode=(int)node[0];
-            double curProb=node[1];
+            double[] edge=maxHeap.poll();
+            int node= (int) edge[0];
+            double curProb= edge[1];
             
-            if(curNode==end_node) return curProb;
+            if(node==end_node) return curProb;
             
-            for(double[] neighbour: graph.get(curNode)){
+            for(double[] neighbour: graph.get(node)){
                 int nextNode=(int) neighbour[0];
-                double nextProb= neighbour[1];
-                double newProb= curProb*nextProb;
+                double newProb= curProb * neighbour[1];
                 
-                if(newProb> maxProb[nextNode]){
-                    maxProb[nextNode]=newProb;
+                if(newProb> allProb[nextNode]){
+                    allProb[nextNode]=newProb;
                     maxHeap.offer(new double[]{nextNode,newProb});
                 }
             }
