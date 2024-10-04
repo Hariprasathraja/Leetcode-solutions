39a6602fe47cd6914819b088d953394e12c385ff
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-10-04 22:44:01 +0530

    Time: 33 ms (6.05%), Space: 58 MB (15.11%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public long dividePlayers(int[] skill) {
         if(skill.length%2!=0) return -1;
         Arrays.sort(skill);
@@ -10,6 +10,33 @@ class Solution {
             res=res+(skill[left]*skill[right]);
             left++;
             right--;
+        }
+        return res;
+    }
+}
+*/
+class Solution {
+    public long dividePlayers(int[] skill) {
+        int n=skill.length;
+        if(n%2!=0) return -1;
+        
+        long totalSum=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i: skill){
+            map.put(i,map.getOrDefault(i,0)+1);
+            totalSum+=i;
+        }
+        long targetSum=totalSum/(n/2);
+        long res=0;
+        for(int i: skill){
+            if(map.get(i)==0) continue;
+            
+            int complement=(int)targetSum-i;
+            if(map.getOrDefault(complement, 0) == 0) return -1;
+            
+            res=res+(i*complement);
+            map.put(i,map.get(i)-1);
+            map.put(complement,map.get(complement)-1);
         }
         return res;
     }
