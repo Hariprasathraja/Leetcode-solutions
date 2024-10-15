7251a02e501eccf238cfd38b21b7168d0db8a84e
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-10-15 21:27:24 +0530

    Time: 7 ms (100.00%), Space: 45.3 MB (70.97%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -2,8 +2,8 @@ class Solution {
     public long minimumSteps(String s) {
         long res=0;
         int oneCount=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1') oneCount++;
+        for(char i:s.toCharArray()){
+            if(i=='1') oneCount++;
             else{
                 if(oneCount>0){
                     res+=oneCount;
