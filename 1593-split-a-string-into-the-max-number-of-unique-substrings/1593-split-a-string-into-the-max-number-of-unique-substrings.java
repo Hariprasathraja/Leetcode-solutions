21dcf6cdd835f8cd22b0fdb5ff6fff48b9271a99
Hari prasath raja B S <115165439+Hariprasathraja@users.noreply.github.com>
2024-10-22 21:15:22 +0530

    Time: 30 ms (86.65%), Space: 45.1 MB (46.91%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,22 +1,21 @@
 class Solution {
- int max = 0;
+    int max = 0;
+    public int maxUniqueSplit(String s) {
+        backTrack(s, 0, new HashSet<>());
+        return max;
+    }
    
-   public int maxUniqueSplit(String s) {
-       backtracking(s, 0, new HashSet<>());
-       return max;
-   }
-   
-   private void backtracking(String s, int start, Set<String> set) {
-       if (start == s.length()) {
-           max = Math.max(max, set.size());
-       } else {
-           for (int i = start + 1; i <= s.length(); i++) {
-               String string = s.substring(start, i);
-               if (set.add(string)) {
-                   backtracking(s, i, set);
-                   set.remove(string);
-               }
-           }
-       }
-   }
+    private void backTrack(String s, int start, Set<String> set) {
+        if(start==s.length()){
+            max=Math.max(max,set.size());
+        }else{
+            for(int i=start+1;i<=s.length();i++){
+                String st=s.substring(start,i);
+                if(set.add(st)){
+                    backTrack(s,i,set);
+                    set.remove(st);
+                }
+            }
+        }
+    }
 }
